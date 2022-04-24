b588d004c087556035cba8a7e5388e064777fe6c
Pramoj N M <pramojnm@gmail.com>
2022-04-24 10:58:16 +0530

    movie app

---
+++ helpers/product-helpers.js
@@ -0,0 +1,53 @@
+var db=require('../config/connection')
+var collection=require('../config/collections')
+const objectId=require('mongodb').ObjectID
+const { ObjectID } = require('bson')
+const { response } = require('express')
+// const { ObjectId } = require('mongodb')
+// const { ObjectId } = require('mongodb')
+module.exports={
+
+    addProduct:(product,callback)=>{
+        
+        db.get().collection('product').insertOne(product).then((data)=>{
+            callback(data.insertedId)
+        })
+    },
+    getAllProducts:()=>{
+        return new Promise(async(resolve,reject)=>{
+            let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            resolve(products)
+        })
+    },
+    deleteProduct:(proId)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).deleteOne({_id:objectId(proId)}).then((response)=>{
+                resolve(response)
+            })
+        })
+    },
+    getProductDetails:(proId)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).findOne({_id:objectId(proId)}).then((product)=>{
+                resolve(product)
+            })
+        })
+    },
+    updateProduct:(proId,proDetails)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION)
+            .updateOne({_id:objectId(proId)},{
+                $set:{
+                    Name:proDetails.Name,
+                    Description:proDetails.Description,
+                    Price:proDetails.Price,
+                    Category:proDetails.Category
+                }
+            }).then((response)=>{
+                resolve()
+            })
+
+            })
+        }
+    
+}
